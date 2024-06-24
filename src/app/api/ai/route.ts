10d6e700e07f399824b6fccda368fa5724bc8c50
Tomás Pérez <tppshaka77@gmail.com>
2024-06-24 09:08:28 -0700

    fix: remove unused api

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,18 +1,18 @@
-import { NextResponse, NextRequest } from 'next/server'
+//import { NextResponse, NextRequest } from 'next/server'
 import * as yup from 'yup';
 import { openai } from '@ai-sdk/openai';
 import { convertToCoreMessages, streamText } from 'ai';
 import { z } from 'zod';
-import gamesRulebookData from "../../../../downloads/rulebooksDownloaded.json";
+//import gamesRulebookData from "../../../../downloads/rulebooksDownloaded.json";
 
-const postSchema = yup.object({
+/* const postSchema = yup.object({
   game: yup.string().required(),
   chatHistory: yup.array().of(yup.object({
     role: yup.string().required(),
     content: yup.string().required(),
   })).required(),
   //complete: yup.boolean().optional().default(false),
-});
+}); */
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -29,9 +29,9 @@ export async function POST(req: Request) {
         description:
           "Get information about the setup instructions from the rulebook, mentioning what page of the rulebook they can find the information on.",
         parameters: z.object({}),
-        execute: async function* () {
+        /* execute: async function* () {
           return gamesRulebookData["Wyrmspan"]
-        },
+        }, */
       },
       askForConfirmation: {
         description: 'Ask the user for confirmation.',
